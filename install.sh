4e0cf873561dbba6df4a34808c08e81fa7f84c83
Alberto Gil de Dios <alberto.gildedios@dynatrace.com>
2025-12-16 13:15:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt install zsh -y
-apt install fzf -y
+# apt install fzf -y   ### apt package of fzf is outdated. clone the repo and install from there instead.
 apt install neofetch -y
 apt install bat -y
 apt install curl -y
